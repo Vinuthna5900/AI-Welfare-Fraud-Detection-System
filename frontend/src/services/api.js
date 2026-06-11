d57ae62287e657bb13498f393688a11afa84732c
Vinuthna5900 <23eg106d04@anurag.edu.in>
2026-06-11 16:06:27 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://ai-welfare-fraud-detection-system-3.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
